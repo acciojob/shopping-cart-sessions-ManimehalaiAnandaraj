2e2a720f44e5da1578080b76c6d7a35e5f527df1
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-04-13 13:34:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -40,7 +40,11 @@ function addToCart(productId) {
     if (productToAdd && !cart.some(product => product.id === productId)) {
         cart.push(productToAdd);
         sessionStorage.setItem('cart', JSON.stringify(cart));   
+         alert("Current cart: " + JSON.stringify(cart)); // Show the entire cart
+        
         renderCart();
+    } else {
+        alert("Product is already in the cart.");
        }
 }
 
